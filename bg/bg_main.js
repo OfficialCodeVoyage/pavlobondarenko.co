789c690a12f33c4bd23171017ebeb865d0fdad05
Pavlo Bondarenko <bondarenkopavloua@yahoo.com>
2024-04-10 22:18:25 -0500

    UPD Bg v3

--- bg/bg_main.js
+++ bg/bg_main.js
@@ -1,7 +1,7 @@
-let NUM_PARTICLES = ( ( ROWS = 200 ) * ( COLS = 300 ) ),
+let NUM_PARTICLES = ( ( ROWS = 330 ) * ( COLS = 300 ) ),
     THICKNESS = Math.pow( 80, 2 ),
     SPACING = 3,
-    MARGIN = 100,
+    MARGIN = 1,
     COLOR = 220,
     DRAG = 0.95,
     EASE = 0.25,
@@ -37,25 +37,25 @@ function init() {
   console.log("resize");
   container = document.getElementById( 'container' );
   canvas = document.createElement( 'canvas' );
-  coords = document.getElementById( 'coords' ); 
+  coords = document.getElementById( 'coords' );
   ctx = canvas.getContext( '2d' );
   man = false;
   tog = true;
-  
+
   list = [];
-  
+
   SPACING = (window.innerWidth - MARGIN * 2) / COLS
   w = canvas.width = COLS * SPACING + MARGIN * 2;
   h = canvas.height = ROWS * SPACING + MARGIN * 2;
   container.style.marginLeft = Math.round( w * -0.5 ) + 'px';
   container.style.marginTop = Math.round( h * -0.5 ) + 'px';
-  
+
   for ( i = 0; i < NUM_PARTICLES; i++ ) {
-    
+
     p = Object.create( particle );
     p.x = p.ox = MARGIN + SPACING * ( i % COLS );
     p.y = p.oy = MARGIN + SPACING * Math.floor( i / COLS );
-    
+
     list[i] = p;
   }
 
@@ -65,18 +65,18 @@ function init() {
     mx = e.clientX - bounds.left;
     my = e.clientY - bounds.top;
     man = true;
-    
+
   });
-  
+
   container.addEventListener( 'mouseout', function(e) {
 
     bounds = container.getBoundingClientRect();
     mx = e.clientX - bounds.left;
     my = e.clientY - bounds.top;
     man = false;
-    
+
   });
-  
+
   container.appendChild( canvas );
 }
 
@@ -84,11 +84,11 @@ function step() {
 
 
 
- 
+
   if(tog){
     if ( !man ) {
-      
-      
+
+
       if(10 * Math.random()> 9){
         t = +new Date() * 0.001;
         mx = Math.floor(w * 0.5 + ( Math.cos( t )* w * 0.3));
@@ -98,11 +98,11 @@ function step() {
 
 
     }
-      
+
     for ( i = 0; i < NUM_PARTICLES; i++ ) {
-      
+
       p = list[i];
-      
+
       d = ( dx = mx - p.x ) * dx + ( dy = my - p.y ) * dy;
       f = -THICKNESS / d;
 
@@ -114,13 +114,13 @@ function step() {
         p.c2 =0;
         p.c3 = 0;
       }
-      else{       
+      else{
         p.c1 = p.c2 = p.c3 = 50;
       }
       if(p.vx > 10 ){
-            p.c2 =255;      
+            p.c2 =255;
       }
-      if( p.vy > 10){       
+      if( p.vy > 10){
         p.c3 = 255;
       }
 
@@ -130,7 +130,7 @@ function step() {
     }
     tog = false;
   } else {
-    
+
     b = ( a = ctx.createImageData( w, h ) ).data;
 
     for ( i = 0; i < NUM_PARTICLES; i++ ) {
@@ -147,6 +147,8 @@ function step() {
   coords.innerHTML = `X=${Math.round(mx)} Y=${Math.round(my)}`;
   requestAnimationFrame( step );
 }
+
+
 
 init();
 step();
